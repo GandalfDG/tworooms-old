e403524dce8f808f04bd52a2b0d79266a3bf5660
Jack Case <jackacase@gmail.com>
2019-05-20 13:18:28 -0400

    created a Card model

--- game/models.py
+++ game/models.py
@@ -8,9 +8,14 @@ class Game(models.Model):
     access_code = models.CharField(max_length=256, unique=True)
 
     # TODO there will be a finite number of game states, use choices option
-    state = models.CharField(max_length=256)
+    state = models.CharField(max_length=256, default="waitingForPlayers")
 
     # a game may consist of either 3 or 5 rounds
     rounds = models.PositiveSmallIntegerField()
 
     current_round = models.PositiveSmallIntegerField()
+
+class Card(models.Model):
+    name = models.CharField(max_length=256)
+    color = models.CharField(max_length=256)
+    image_url = models.URLField(max_length=256)
