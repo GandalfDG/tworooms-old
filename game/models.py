24005eb870c7e5788bb6a6894bcc0b96a3f9657c
Jack Case <jackacase@gmail.com>
2019-05-20 12:49:09 -0400

    setting up an initial game model to keep track of each game

--- game/models.py
+++ game/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Game(models.Model):
+    # the access code will be used to connect all players to a game instance
+    access_code = models.CharField(max_length=256, unique=True)
+
+    # TODO there will be a finite number of game states, use choices option
+    state = models.CharField(max_length=256)
+
+    # a game may consist of either 3 or 5 rounds
+    rounds = models.PositiveSmallIntegerField()
+
+    current_round = models.PositiveSmallIntegerField()
