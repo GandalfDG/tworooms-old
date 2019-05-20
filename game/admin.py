e872be0c429b0c7c732a8c8d91483e3ca2a456b6
Jack Case <jackacase@gmail.com>
2019-05-20 15:48:34 -0400

    adding more models and their associated admin things also added __str__ methods to some models

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Card, Playset
+
 # Register your models here.
+
+admin.site.register(Card)
+admin.site.register(Playset)
--- game/models.py
+++ game/models.py
@@ -8,14 +8,48 @@ class Game(models.Model):
     access_code = models.CharField(max_length=256, unique=True)
 
     # TODO there will be a finite number of game states, use choices option
-    state = models.CharField(max_length=256, default="waitingForPlayers")
+    state = models.CharField(max_length=256)
 
     # a game may consist of either 3 or 5 rounds
     rounds = models.PositiveSmallIntegerField()
 
     current_round = models.PositiveSmallIntegerField()
 
+    def __str__(self):
+        return 'game ' + self.access_code
+
+class Player(models.Model):
+    name = models.CharField(max_length=256)
+    is_moderator = models.BooleanField(default=False)
+    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+    role = models.ForeignKey('Card', on_delete=models.CASCADE)
+
+
+class Playset(models.Model):
+    name = models.CharField(max_length=256)
+    cards = models.ManyToManyField('Card')
+
+    def __str__(self):
+        return self.name
+
+
 class Card(models.Model):
     name = models.CharField(max_length=256)
-    color = models.CharField(max_length=256)
-    image_url = models.URLField(max_length=256)
+
+    CARD_COLORS = [
+        ('RD', 'Red'),
+        ('BL', 'Blue'),
+        ('GR', 'Gray'),
+        ('PR', 'Purple'),
+        ('PK', 'Pink'),
+        ('GN', 'Green'),
+        ('YL', 'Yellow'),
+    ]
+
+    color = models.CharField(max_length=2, choices=CARD_COLORS)
+
+    tagline = models.TextField(null=True)
+    full_description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name + ' (' + self.color + ')'
