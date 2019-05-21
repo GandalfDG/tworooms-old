28e09f3b672708f55be7328f51f6242381ecc1ef
Jack Case <jackacase@gmail.com>
2019-05-20 20:29:10 -0400

    added games to the admin console

--- game/admin.py
+++ game/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Card, Playset
+from .models import Card, Playset, Game
 
 # Register your models here.
 
 admin.site.register(Card)
 admin.site.register(Playset)
+admin.site.register(Game)
