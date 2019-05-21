8df5cb03d0153ff7bc2f9de392d6157e781e6474
Jack Case <jackacase@gmail.com>
2019-05-20 23:33:37 -0400

    learned something new about python conventions

--- game/game_logic.py
+++ game/game_logic.py
@@ -8,7 +8,7 @@ class GameLogic:
         code = ''
         possible = 'abcdefghjkmnpqrstuvwxyz23456789'
 
-        for i in range(0, 6):
+        for _ in range(0, 6):
             code += random.choice(possible)
 
         return code
