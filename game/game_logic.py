4c016bf3b689adab18369983b9c3d684fe2398fa
Jack Case <jackacase@gmail.com>
2019-05-21 20:14:36 -0400

    game logic module didn't need to be a class

--- game/game_logic.py
+++ game/game_logic.py
@@ -2,13 +2,12 @@ import random
 
 random.seed()
 
-class GameLogic:
 
-    def generate_access_code(self):
-        code = ''
-        possible = 'abcdefghjkmnpqrstuvwxyz23456789'
+def generate_access_code():
+    code = ''
+    possible = 'abcdefghjkmnpqrstuvwxyz23456789'
 
-        for _ in range(0, 6):
-            code += random.choice(possible)
+    for _ in range(0, 6):
+        code += random.choice(possible)
 
-        return code
+    return code
