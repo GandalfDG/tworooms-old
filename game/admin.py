60ee88c86ac742084df644fb768b8c45eeda79a5
Jack Case <jackacase@gmail.com>
2019-05-22 14:16:44 -0400

    added players to the admin site

--- game/admin.py
+++ game/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Card, Playset, Game
+from .models import Card, Playset, Game, Player
 
 # Register your models here.
 
 admin.site.register(Card)
 admin.site.register(Playset)
 admin.site.register(Game)
+admin.site.register(Player)
