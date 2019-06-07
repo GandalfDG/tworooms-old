3bce147c3dac80b1a644c9e4e086869bbb3f0592
Jack Case <jackacase@gmail.com>
2019-06-07 00:08:09 -0400

    added general flow design comment to app.js

--- game/static/game/app.js
+++ game/static/game/app.js
@@ -1,3 +1,11 @@
+/*
+ *  1. send a POST with a player name to create a new game
+ *  2. receive a response with the newly created game
+ *  3. switch to the lobby and poll for players being added
+ *  4. player 1 will start the game with a POST 
+ *  5. all players will be switched to the game screen
+ */
+
 var page = "home"
 
 var app = new Vue({
