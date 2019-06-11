9110658668b33b6e15eda20eaa65a770bd221c38
Jack Case <jackacase@gmail.com>
2019-06-10 20:37:44 -0400

    handling of newgame and join game clicks using router.push

--- frontend/src/components/LandingPage.vue
+++ frontend/src/components/LandingPage.vue
@@ -1,13 +1,20 @@
 <template>
 <div>
     <h1>Landing Page</h1>
-    <button @click="type="button" class="btn btn-primary">New Game</button>
-    <button type="button" class="btn btn-outline-primary">Join Game</button>
+    <button @click="newGame" type="button" class="btn btn-primary">New Game</button>
+    <button @click="joinGame" type="button" class="btn btn-outline-primary">Join Game</button>
 </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    newGame () {
+      this.$router.push('/new/')
+    },
+    joinGame () {
+      this.$router.push('/join')
+    }
+  }
 }
 </script>
