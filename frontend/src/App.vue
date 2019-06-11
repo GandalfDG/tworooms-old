329f35fab68c9142dcb5a4d3a14a98f894e4cffa
Jack Case <jackacase@gmail.com>
2019-06-10 20:25:46 -0400

    Working on the frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="col-md-6 offset-md-3 border">
     <img src="./assets/logo.png">
     <router-view/>
   </div>
---
+++ frontend/src/components/LandingPage.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <h1>Landing Page</h1>
+    <button @click="type="button" class="btn btn-primary">New Game</button>
+    <button type="button" class="btn btn-outline-primary">Join Game</button>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
