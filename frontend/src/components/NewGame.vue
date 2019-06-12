fec533e5b3f4bb09b2d39ff0ae8a131e48326287
Jack Case <jackacase@gmail.com>
2019-06-12 19:32:41 -0400

    began work on the newgame component

---
+++ frontend/src/components/NewGame.vue
@@ -0,0 +1,14 @@
+<template>
+    <form>
+        <div class="form-group">
+            <label for="playerName">Enter Your Name</label>
+            <input type="text" class="form-control" id="playerName" placeholder="John Smith">
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import LandingPage from '@/components/LandingPage'
+import NewGame from '@/components/NewGame'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'LandingPage',
       component: LandingPage
+    },
+    {
+      path: '/new/',
+      name: 'NewGame',
+      component: NewGame
     }
   ]
 })
