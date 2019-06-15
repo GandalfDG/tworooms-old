68e1bca6f0a802b37542310fe296d956355a56ee
Jack Case <jackacase@gmail.com>
2019-06-15 10:22:41 -0400

    trying to figure out how to get the response object out of promiseland

--- frontend/src/components/NewGame.vue
+++ frontend/src/components/NewGame.vue
@@ -1,17 +1,20 @@
 <template>
-  <form>
-    <div class="form-group">
-      <label for="playerName">Enter Your Name</label>
-      <input
-        v-model="name"
-        type="text"
-        class="form-control"
-        id="playerName"
-        placeholder="John Smith"
-      >
-    </div>
-    <button @click="createGame" type="button" class="btn btn-primary">Create Game</button>
-  </form>
+  <div>
+    <form>
+      <div class="form-group">
+        <label for="playerName">Enter Your Name</label>
+        <input
+          v-model="name"
+          type="text"
+          class="form-control"
+          id="playerName"
+          placeholder="John Smith"
+        >
+      </div>
+      <button @click="createGame" type="button" class="btn btn-primary">Create Game</button>
+    </form>
+    <h1 v-if="accessCode">{{accessCode}}</h1>
+  </div>
 </template>
 
 <script>
@@ -19,14 +22,18 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      name: ''
+      name: '',
+      accessCode: ''
     }
   },
   methods: {
     createGame () {
-      axios.post('http://localhost:8000/newgame/', {'player_name': this.name}).then(function (response) {
-        console.log(response)
-      })
+      axios
+        .post('http://localhost:8000/newgame/', { player_name: this.name })
+        .then(updateAccessCode(response))
+    },
+    updateAccessCode (response) {
+      this.accessCode = response.data.access_code
     }
   }
 }
