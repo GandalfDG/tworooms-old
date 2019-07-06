37c68e202049e93170a9804792bdfdc011c92092
Jack Case <jackacase@gmail.com>
2019-07-06 10:56:21 -0400

    added a join URL which allows a player to join an existing game

--- game/views.py
+++ game/views.py
@@ -14,7 +14,7 @@ def game(request):
     receive a player name in a POST to create a new game
     return the newly created game object
     """
-    if request.method == 'POST':
+    if request.method == 'POST': #create a new game
         game = Game()
         code = game.new_game()
         playername = request.data['player_name']
@@ -25,7 +25,7 @@ def game(request):
         serializer = GameSerializer(game)
         return Response(serializer.data)
     
-    elif request.method == 'GET':
+    elif request.method == 'GET': #retrieve info for an existing game
         if 'access_code' in request.query_params:
             game = Game.objects.get(access_code=request.query_params['access_code'])
             serializer = GameSerializer(game)
@@ -33,6 +33,21 @@ def game(request):
         else:
             return Response()
 
+@api_view(['POST'])
+def join(request):
+    """
+    receive a player name and access code in a POST to join
+    an existing game
+    """
+    if request.method == 'POST':
+        playername = request.data['player_name']
+        player = Player(name=playername)
+        accesscode = request.data['access_code']
+        player.join_game(accesscode)
+        game = Game.objects.get(access_code=accesscode)
+        serializer = GameSerializer(game)
+        return Response(serializer.data)
+        
 
 @api_view(['GET'])
 def api_root(request, format=None):
--- tworooms/urls.py
+++ tworooms/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from game.views import api_root, game
+from game.views import api_root, game, join
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', api_root, name="API Root"),
     path('game/', game, name='game'),
+    path('join/', join, name='join'),
 ]
